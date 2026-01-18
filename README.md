a6bf0b8852863ae1079464f7c989efbcf1e068a1
Mian <docgoosenn@gmail.com>
2026-01-18 00:14:47 -0800

    Set up auto publishing, Curseforge doesn't work for now

--- README.md
+++ README.md
@@ -1,20 +1,41 @@
 # Lifesteal | 💘
 ...is a mod for Hytale that takes from the popular 'Lifesteal' mod from Minecraft with its own spin on it to better fit Hytale!
 
+<img src="https://i.imgur.com/zvR3WIU.gif">
+
 ## Features | ➕
 **Every feature mentioned in this mod is configurable.**
 
 - Kill players (or entities if configured) to gain a configurable amount of permanent health!
-- When players lose all their max health, they are banned from the server
-- A new special weapon that siphons health from your enemies to heal you when you attack them!
-- Two ways to revive players!
-  - Through a one use item that allows you to revive anyone you want
-  - Automatic revive timer (configurable!)
-- Special ores underground that allow you to craft heart crystals to gain even more permanent health! Mauhahaha!
 - Configure the amount of max and min health for players!
+- When players lose all their max health, they are banned from the server
+- A new special weapon that siphons health from your enemies to heal you when you attack them! (WORK IN PROGRESS)
+- Two ways to revive players!
+  - Through a one use item that allows you to revive anyone you want (WORK IN PROGRESS)
+  - Automatic revive timer (configurable!)
+- Special ores underground that allow you to craft heart crystals to gain even more permanent health! Mauhahaha! (WORK IN PROGRESS)
+
+## Config | ⚙️
+
+- StartAmount : The amount of extra stolen health people start with
+- StealAmount : The amount of health people steal when they kill an entity
+- CapAmount : Limits the amount of stolen health a player can have, set to -1 to disable
+- MinAmount : Limits the amount of debt a player can have in stolen health
+- BanAtMin : Whether to ban players when they reach the debt limit or not
+- AutoReviveAfterTime : Automatically revives players after a certain amount of seconds
+- LoseHealthOnEnvDeath : Whether to lose health on environment death
+- LoseHealthOnPlayerDeath : Whether to lose health on player death
+- LoseHealthOnEntityDeath : Whether to lose health on entity death
+- EarnLifeFromNonPlayers : Whether to earn stolen health from non-players
+- CanEarnLifeFromEntitiesEvenIfTheyDontHaveEnough : Whether to earn stolen health from entities even if they reached the debt limit
+
 
 ## FAQ | ❓
 - Does this conflict with mods that modify permanent health?
   - Nope!
 
-## Credits | 👏
+## BUGS/FEATURES | 🐛
+- Helppp i found bug where do i go?!!!!
+  - Head over to the source link, and write up an issue detailing the bug alongside a server log!
+- I have a cool idea!
+  - Head over to the source link, and write up an issue detailing the suggestion!
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import groovy.json.JsonOutput
+
 plugins {
     id("java-library")
     id("com.gradleup.shadow") version "9.3.1"
@@ -10,6 +12,10 @@ description = findProperty("pluginDescription") as String
 
 var game_build = findProperty("gameBuild") as String
 var hytalePath = System.getProperty("user.home") + "/AppData/Roaming/Hytale/install/release/package/game/${game_build}/Server/HytaleServer.jar"
+var modtale_id = findProperty("modtale_id") as String
+var curseforge_id = findProperty("curseforge_id") as String
+var orbis_id = findProperty("orbis_id") as String
+var release_channel = findProperty("release_channel") as String
 
 repositories {
     mavenLocal()
@@ -82,6 +88,192 @@ tasks {
         dependsOn(shadowJar)
     }
 }
+
+tasks.register("publishAll") {
+    group = "publishing"
+    description = "Publishes to all platforms"
+
+    dependsOn(
+        "publishToModTale",
+        "publishToCurseForge"
+    )
+}
+
+tasks.register<Exec>("publishToModTale") {
+    group = "publishing"
+    description = "Publishes the built jar to Modtale.net."
+
+    dependsOn(tasks.named("shadowJar"))
+
+    val curlExe = if (org.gradle.internal.os.OperatingSystem.current().isWindows) {
+        "C:/Windows/System32/curl.exe"
+    } else {
+        "curl"
+    }
+
+    doFirst {
+        val jarFile = layout.buildDirectory
+            .file("libs/${project.name}-${project.version}.jar")
+            .get()
+            .asFile
+
+        if (!jarFile.exists()) {
+            throw GradleException("Error: Jar file not found: $jarFile")
+        }
+
+        val apiKey = project.findProperty("modtale_token") as String
+        val projectId = modtale_id
+
+        if (apiKey.isNullOrBlank() || projectId.isNullOrBlank()) {
+            throw GradleException("Error: modtale_token and modtale_id var is required.")
+        }
+
+
+        val changelogFile = project.file("changelog.md")
+
+        if (!changelogFile.exists()) {
+            throw GradleException("changelog.md not found")
+        }
+
+        val changelogText = changelogFile
+            .takeIf { it.exists() }
+            ?.readText(Charsets.UTF_8)
+            ?: "Release ${project.version}"
+
+        commandLine(
+            curlExe,
+            "-X", "POST",
+            "https://api.modtale.net/api/v1/projects/$projectId/versions",
+            "-H", "X-MODTALE-KEY: $apiKey",
+            "-F", "file=@${jarFile.absolutePath}",
+            "-F", "versionNumber=${project.version}",
+            // RELEASE | BETA | ALPHA
+            "-F", "channel=$release_channel",
+            // Comma-separated list (Release 1.0, Release 1.1)
+            "-F", "gameVersions=1.0-SNAPSHOT",
+            // Markdown string
+            "-F", "changelog=$changelogText"
+        )
+    }
+}
+
+
+// doesn't work for now
+tasks.register<Exec>("publishToCurseForge") {
+    group = "publishing"
+    description = "Publishes the built Hytale mod to CurseForge."
+    dependsOn("shadowJar")
+
+    val curlExe = if (org.gradle.internal.os.OperatingSystem.current().isWindows) {
+        "C:/Windows/System32/curl.exe"
+    } else {
+        "curl"
+    }
+
+    doFirst {
+        val apiToken = project.findProperty("curseforge_token") as String
+
+        val jarFile = layout.buildDirectory
+            .file("libs/${project.name}-${project.version}.jar")
+            .get()
+            .asFile
+
+        val changelogFile = project.file("changelog.md")
+
+        if (apiToken.isNullOrBlank() || curseforge_id.isNullOrBlank()) {
+            throw GradleException("curseforge_token and curseforge_id are required.")
+        }
+
+        if (!jarFile.exists()) {
+            throw GradleException("Jar file not found: $jarFile")
+        }
+
+        val changelogText = changelogFile
+            .takeIf { it.exists() }
+            ?.readText(Charsets.UTF_8)
+            ?: "Release ${project.version}"
+
+
+        val metadata = mapOf(
+            "changelog" to changelogText,
+            "changelogType" to "markdown",
+            "releaseType" to release_channel.lowercase(),
+            "gameVersions" to listOf("Early Access")
+        )
+        val metadataJson = JsonOutput.toJson(metadata)
+
+
+        commandLine(
+            curlExe,
+            "-X", "POST",
+            "-H", "X-Api-Token: $apiToken",
+
+            "-F", "metadata=$metadataJson;type=application/json",
+            "https://hytale.curseforge.com/api/projects/$curseforge_id/upload-file",
+        )
+    }
+}
+
+fun String.jsonEscape(): String =
+    this
+        .replace("\\", "\\\\")
+        .replace("\"", "\\\"")
+        .replace("\r", "")
+        .replace("\n", "\\n")
+
+//tasks.register<Exec>("publishToOrbis") {
+//    group = "publishing"
+//    description = "Publishes the built Hytale mod to Orbis."
+//    dependsOn("shadowJar")
+//
+//    val curlExe = if (org.gradle.internal.os.OperatingSystem.current().isWindows) {
+//        "C:/Windows/System32/curl.exe"
+//    } else {
+//        "curl"
+//    }
+//
+//    doFirst {
+//        val apiToken = System.getenv("ORBIS_API_TOKEN")
+//
+//        val jarFile = layout.buildDirectory
+//            .file("libs/${project.name}-${project.version}.jar")
+//            .get()
+//            .asFile
+//
+//        val changelogFile = project.file("changelog.md")
+//
+//        if (apiToken.isNullOrBlank() || orbis_id.isNullOrBlank()) {
+//            throw GradleException("ORBIS_API_TOKEN and ORBIS_PROJECT_ID are required.")
+//        }
+//
+//        if (!jarFile.exists()) {
+//            throw GradleException("Jar file not found: $jarFile")
+//        }
+//
+//        val changelogText = changelogFile
+//            .takeIf { it.exists() }
+//            ?.readText(Charsets.UTF_8)
+//            ?: "Release ${project.version}"
+//
+//        commandLine(
+//            curlExe,
+//            "-X", "POST",
+//            "https://minecraft.curseforge.com/api/v1/projects/$orbis_id/upload-file",
+//            "-H", "X-Api-Token: $apiToken",
+//
+//            // File upload
+//            "-F", "file=@${jarFile.absolutePath}",
+//
+//            // Metadata
+//            "-F", "changelog=$changelogText",
+//            "-F", "changelogType=markdown",
+//            "-F", "releaseType=${release_channel}",
+//
+//            // Hytale game version ID(s)
+//            "-F", "gameVersions=Early Access"
+//        )
+//    }
+//}
 
 // Configure Java toolchain
 java {
